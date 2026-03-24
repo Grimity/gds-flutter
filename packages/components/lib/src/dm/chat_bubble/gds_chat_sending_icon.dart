9d556838457c16f372fe7cf952404a95241c715e
wonyong <wanyong0919@naver.com>
2026-03-24 19:05:14 +0900

    refactor: Message 아이콘 적용

--- packages/components/lib/src/dm/chat_bubble/gds_chat_sending_icon.dart
+++ packages/components/lib/src/dm/chat_bubble/gds_chat_sending_icon.dart
@@ -1,12 +1,11 @@
 part of '../gds_chat_bubble.dart';
 
-/// TODO Sending 아이콘 Atomic 아이콘에 추가 후 변경 필요
 class GdsChatSendingIcon extends StatelessWidget {
   const GdsChatSendingIcon({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GdsIcon.galleryFill.build(
+    return GdsIcon.message.build(
       width: GdsIconSize.v16,
       height: GdsIconSize.v16,
       color: context.gdsColors.icon.graySubtler,
