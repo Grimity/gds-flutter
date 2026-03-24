fcba6cf4cfe49fb2d1451f525c367e2008e1c8b4
wonyong <wanyong0919@naver.com>
2026-03-24 17:47:04 +0900

    feat: GdsDevice helper 및 기기 타입 판별 로직 추가

---
+++ packages/foundation/lib/src/helper/gds_device.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+import '../gds_screen.dart';
+
+enum GdsDeviceType {
+  mobile,
+  tablet,
+}
+
+extension GdsDevice on BuildContext {
+  double get screenWidth => MediaQuery.sizeOf(this).width;
+
+  GdsDeviceType get gdsDeviceType =>
+      screenWidth >= GdsScreen.tableMinWidth ? GdsDeviceType.tablet : GdsDeviceType.mobile;
+
+  bool get isTablet => gdsDeviceType == GdsDeviceType.tablet;
+
+  bool get isMobile => gdsDeviceType == GdsDeviceType.mobile;
+}
